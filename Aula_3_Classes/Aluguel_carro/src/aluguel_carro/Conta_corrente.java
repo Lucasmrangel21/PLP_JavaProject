f68e6229e25782293351e2dd42e50879321641ed
Lucas <lucas.mrangel@sempreceub.com>
2020-05-04 12:43:04 -0300

    Nova tarefa de classes

---
+++ Aula_3_Classes/Aluguel_carro/src/aluguel_carro/Conta_corrente.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aluguel_carro;
+
+/**
+ *
+ * @author Acer
+ */
+public class Conta_corrente {
+    int numeroConta;
+    String nomeBanco;
+    String nomeAgencia;
+    String nomeCliente;
+    double saldo;
+}
